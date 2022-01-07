3cc6c886fbe266a5564e5d74b8f7faa9870ff945
Vũ Quang Minh <jokerdarkknight@protonmail.com>
2022-01-08 02:37:34 +0700

    Add Mobile Navigation

--- src/el/Navigations/mobileNav.vue
+++ src/el/Navigations/mobileNav.vue
@@ -1,21 +1,31 @@
 <script setup>
-import FilmIcon from '../../assets/img/film.svg';
-import HomeIcon from '../../assets/img/home.svg';
-import SingleUserIcon from '../../assets/img/user.svg';
+import HomeIcon from "../../assets/img/home.svg";
+import FilmIcon from "../../assets/img/film.svg";
+import SingleUserIcon from "../../assets/img/user.svg";
+import SearchIcon from "../../assets/img/search.svg";
+import HamburgerIcon from "../../assets/img/hamburger.svg";
 </script>
 <template>
   <div class="m-nav">
     <router-link to="/" class="item">
-      <FilmIcon />
-      <span>Phim</span>
-    </router-link>
-    <router-link to="/" class="item">
       <HomeIcon />
       <span>Trang chủ</span>
     </router-link>
-    <router-link to="/" class="item">
+    <router-link to="/films" class="item">
       <FilmIcon />
-      <span>Phim</span>
+      <span>Phim mới</span>
+    </router-link>
+    <router-link to="/account" class="item">
+      <SingleUserIcon />
+      <span>Tài khoản</span>
+    </router-link>
+    <router-link to="/search" class="item">
+      <SearchIcon />
+      <span>Tìm kiếm</span>
+    </router-link>
+    <router-link to="/more" class="item more">
+      <HamburgerIcon />
+      <span>Thêm</span>
     </router-link>
   </div>
 </template>
