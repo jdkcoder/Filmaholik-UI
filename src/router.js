3dbc545c041e8ade8440c7d225db4f60e62893fd
Vũ Quang Minh <jokerdarkknight@protonmail.com>
2022-01-18 21:38:44 +0700

    fix problem display on mobile for film details page

--- src/router.js
+++ src/router.js
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./views/Home/Home.vue";
-import Login from "./views/Auth/Login.vue";
 
-import FilmDetails from "./views/Films/Details/fullDetails.vue";
+import Login from "./views/Auth/Login.vue";
+import Register from "./views/Auth/Register.vue";
+import Recover from "./views/Auth/Recover.vue";
+
+import filmDetails from "./views/Films/Details/fullDetails.vue";
+import castDetails from "./views/Films/Details/castDetails.vue";
+import directorDetails from "./views/Films/Details/fullDetails.vue";
 import Player from "./views/Films/Player.vue";
 
 const routes = [
@@ -10,15 +15,36 @@ const routes = [
     path: "/",
     component: Home,
     name: "Home",
-  },{
+  },
+  {
+    path: "/register",
+    component: Register,
+    name: "Register",
+  },
+  {
     path: "/login",
     component: Login,
     name: "Login",
   },
   {
+    path: "/recovery",
+    component: Recover,
+    name: "Recover",
+  },
+  {
     path: "/film/",
-    component: FilmDetails,
+    component: filmDetails,
     name: "Film Details",
+  },
+  {
+    path: "/cast/",
+    component: castDetails,
+    name: "Cast Details",
+  },
+  {
+    path: "/director/",
+    component: directorDetails,
+    name: "Director Details",
   },
   {
     path: "/play/",
--- src/views/Films/Details/fullDetails.vue
+++ src/views/Films/Details/fullDetails.vue
@@ -44,9 +44,11 @@ import DisLikeIcon from "../../../assets/img/dislike.svg";
               </div>
             </div>
           </div>
-          <div class="btn-short-info"
-              data-aos="fade-zoom-in"
-              data-aos-delay="1000">
+          <div
+            class="btn-short-info"
+            data-aos="fade-zoom-in"
+            data-aos-delay="1000"
+          >
             <button
               class="show-full-info"
               type="button"
@@ -62,7 +64,11 @@ import DisLikeIcon from "../../../assets/img/dislike.svg";
         <a class="play-trailer"><TrianglePlayIcon /></a>
       </div>
       <div class="seasons-parts-top">
-        <div class="seasons-parts-list" data-aos="fade-left" data-aos-delay="700">
+        <div
+          class="seasons-parts-list"
+          data-aos="fade-left"
+          data-aos-delay="700"
+        >
           <ul>
             <li class="season active">Mùa 1</li>
             <li>Mùa 2</li>
@@ -122,17 +128,18 @@ import DisLikeIcon from "../../../assets/img/dislike.svg";
       <img
         src="https://i.imgur.com/aLXNzNH.jpg"
         class="img-top"
-        data-aos="fade-zoom-in" data-aos-delay="200"
+        data-aos="fade-zoom-in"
+        data-aos-delay="200"
       />
     </div>
   </section>
 
-  <section id="full-details" class="full-details" v-if="isShow">
+  <section id="full-details" class="full-details" v-if="isMobile()">
     <div class="titles" data-aos="zoom-out" data-aos-delay="200">
       <h3 class="translated-name">Thợ săn quái vật</h3>
       <h5 class="original-name">The Witcher</h5>
     </div>
-    <div class="genres"  data-aos="flip-up" data-aos-delay="400">
+    <div class="genres" data-aos="flip-up" data-aos-delay="400">
       <a href="#!">Thể loại 1</a>
       <a href="#!">Thể loại 2</a>
       <a href="#!">Thể loại 3</a>
@@ -224,7 +231,8 @@ import DisLikeIcon from "../../../assets/img/dislike.svg";
           clickable: true,
         }"
         :navigation="true"
-        data-aos="fade-zoom-in" data-aos-delay="1200"
+        data-aos="fade-zoom-in"
+        data-aos-delay="1200"
       >
         <swiper-slide>
           <img class="movie-thumb" src="https://i.imgur.com/2BFyWFi.png" />
@@ -265,21 +273,27 @@ import DisLikeIcon from "../../../assets/img/dislike.svg";
       </swiper>
     </div>
     <div class="related" style="margin-bottom: 10rem">
-      <strong class="rf-title" data-aos="fade-right" data-aos-delay="1300"> Phim cùng thể loại </strong>
+      <strong class="rf-title" data-aos="fade-right" data-aos-delay="1300">
+        Phim cùng thể loại
+      </strong>
     </div>
   </section>
 </template>
 
 <script>
+import SwiperCore, { Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import "swiper/css";
 import "swiper/css/pagination";
-import SwiperCore, { Navigation } from "swiper";
+
 import AOS from "aos";
 import "aos/dist/aos.css";
+
 SwiperCore.use([Navigation]);
 
 AOS.init();
+
+var check = false;
 export default {
   components: {
     Swiper,
@@ -295,27 +309,46 @@ export default {
     };
   },
   methods: {
+    isMobile() {
+      (function (a) {
+        if (
+          /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+            a
+          ) ||
+          /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+            a.substr(0, 4)
+          )
+        )
+          check = true;
+      })(navigator.userAgent || navigator.vendor || window.opera);
+      return check;
+    },
+
     showFullDetail() {
-      this.isShow = !this.isShow;
+      check = !check;
       this.textShow.clicked = !this.textShow.clicked;
       this.textShow.original = !this.textShow.original;
-      setTimeout(() => {
-        var element = document.getElementById("full-details");
-        var headerOffset = 2;
-        var elementPosition = element.getBoundingClientRect().top;
-        var offsetPosition =
-          elementPosition + window.pageYOffset - headerOffset;
+      if (check) {
+        setTimeout(() => {
+          var element = document.getElementById("full-details");
+          var headerOffset = 2;
+          var elementPosition = element.getBoundingClientRect().top;
+          var offsetPosition =
+            elementPosition + window.pageYOffset - headerOffset;
 
-        window.scrollTo({
-          top: offsetPosition,
-          behavior: "smooth",
-        });
-      }, 200);
+          window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth",
+          });
+        }, 200);
+      } else {
+        return false;
+      }
     },
-    mounted() {},
   },
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "../../../assets/styles/child/film/fullDetails";
 </style>
